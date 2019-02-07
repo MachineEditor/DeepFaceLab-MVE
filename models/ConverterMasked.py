0c1638e1688fdb619478442d55d04aff15600e26
iperov <lepersorium@gmail.com>
2019-02-07 11:14:41 +0400

    fix strange opencv behaviour with border_transparent

--- models/ConverterMasked.py
+++ models/ConverterMasked.py
@@ -120,7 +120,7 @@ class ConverterMasked(ConverterBase):
         prd_face_mask_a   = np.expand_dims (prd_face_mask_a_0, axis=-1)
         prd_face_mask_aaa = np.repeat (prd_face_mask_a, (3,), axis=-1)
 
-        img_prd_face_mask_aaa = cv2.warpAffine( prd_face_mask_aaa, face_output_mat, img_size, np.zeros(img_bgr.shape, dtype=float), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4 )
+        img_prd_face_mask_aaa = cv2.warpAffine( prd_face_mask_aaa, face_output_mat, img_size, np.zeros(img_bgr.shape, dtype=np.float32), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4 )
         img_prd_face_mask_aaa = np.clip (img_prd_face_mask_aaa, 0.0, 1.0)
             
         img_face_mask_aaa = img_prd_face_mask_aaa
@@ -148,7 +148,7 @@ class ConverterMasked(ConverterBase):
                 out_img = img_face_mask_aaa   
 
             if self.raw_mode == 'predicted-only':
-                out_img = cv2.warpAffine( prd_face_bgr, face_output_mat, img_size, np.zeros(out_img.shape), cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4, cv2.BORDER_TRANSPARENT )
+                out_img = cv2.warpAffine( prd_face_bgr, face_output_mat, img_size, np.zeros(out_img.shape, dtype=np.float32), cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4, cv2.BORDER_TRANSPARENT )
                 
         else:
             if maxregion.size != 0:
@@ -189,15 +189,16 @@ class ConverterMasked(ConverterBase):
                             img_face_mask_flatten_aaa = cv2.dilate(img_face_mask_flatten_aaa, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(-ero,-ero)), iterations = 1 )
 
                     if self.clip_hborder_mask_per > 0: #clip hborder before blur
-                        prd_hborder_rect_mask_a = np.ones ( prd_face_mask_a.shape, dtype=prd_face_mask_a.dtype)        
+                        prd_hborder_rect_mask_a = np.ones ( prd_face_mask_a.shape, dtype=np.float32)        
                         prd_border_size = int ( prd_hborder_rect_mask_a.shape[1] * self.clip_hborder_mask_per )
                         prd_hborder_rect_mask_a[:,0:prd_border_size,:] = 0
-                        prd_hborder_rect_mask_a[:,-prd_border_size:,:] = 0
+                        prd_hborder_rect_mask_a[:,-prd_border_size:,:] = 0                        
                         prd_hborder_rect_mask_a = np.expand_dims(cv2.blur(prd_hborder_rect_mask_a, (prd_border_size, prd_border_size) ),-1)
-
-                        img_prd_hborder_rect_mask_a = cv2.warpAffine( prd_hborder_rect_mask_a, face_output_mat, img_size, np.zeros(img_bgr.shape, dtype=np.float32), cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4, cv2.BORDER_TRANSPARENT )
+   
+                        img_prd_hborder_rect_mask_a = cv2.warpAffine( prd_hborder_rect_mask_a, face_output_mat, img_size, np.zeros(img_bgr.shape, dtype=np.float32), cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4 )
                         img_prd_hborder_rect_mask_a = np.expand_dims (img_prd_hborder_rect_mask_a, -1)
                         img_mask_blurry_aaa *= img_prd_hborder_rect_mask_a
+                            
                         if debug:
                             debugs += [img_mask_blurry_aaa.copy()]
                             
@@ -221,21 +222,21 @@ class ConverterMasked(ConverterBase):
                         if debug:
                             debugs += [ cv2.warpAffine( prd_face_bgr, face_output_mat, img_size, np.zeros(img_bgr.shape, dtype=np.float32), cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4, cv2.BORDER_TRANSPARENT ) ]
                             
-                        hist_mask_a = np.ones ( prd_face_bgr.shape[:2] + (1,) , dtype=prd_face_bgr.dtype)
+                        hist_mask_a = np.ones ( prd_face_bgr.shape[:2] + (1,) , dtype=np.float32)
                             
                         if self.masked_hist_match:
                             hist_mask_a *= prd_face_mask_a
                         
-                        hist_match_1 = prd_face_bgr*hist_mask_a + (1.0-hist_mask_a)* np.ones ( prd_face_bgr.shape[:2] + (1,) , dtype=prd_face_bgr.dtype) 
+                        hist_match_1 = prd_face_bgr*hist_mask_a + (1.0-hist_mask_a)* np.ones ( prd_face_bgr.shape[:2] + (1,) , dtype=np.float32) 
                         hist_match_1[ hist_match_1 > 1.0 ] = 1.0
                         
-                        hist_match_2 = dst_face_bgr*hist_mask_a + (1.0-hist_mask_a)* np.ones ( prd_face_bgr.shape[:2] + (1,) , dtype=prd_face_bgr.dtype) 
+                        hist_match_2 = dst_face_bgr*hist_mask_a + (1.0-hist_mask_a)* np.ones ( prd_face_bgr.shape[:2] + (1,) , dtype=np.float32) 
                         hist_match_2[ hist_match_1 > 1.0 ] = 1.0
 
                         prd_face_bgr = image_utils.color_hist_match(hist_match_1, hist_match_2, self.hist_match_threshold )
                             
                     if self.mode == 'hist-match-bw':
-                        prd_face_bgr = prd_face_bgr.astype(np.float32)
+                        prd_face_bgr = prd_face_bgr.astype(dtype=np.float32)
 
                     out_img = cv2.warpAffine( prd_face_bgr, face_output_mat, img_size, out_img, cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4, cv2.BORDER_TRANSPARENT )
             
@@ -251,7 +252,7 @@ class ConverterMasked(ConverterBase):
                             debugs += [out_img.copy()]
 
                         out_img = cv2.seamlessClone( (out_img*255).astype(np.uint8), (img_bgr*255).astype(np.uint8), (img_face_mask_flatten_aaa*255).astype(np.uint8), (masky,maskx) , cv2.NORMAL_CLONE )
-                        out_img = out_img.astype(np.float32) / 255.0
+                        out_img = out_img.astype(dtype=np.float32) / 255.0
                         
                         if debug:
                             debugs += [out_img.copy()]
@@ -280,7 +281,7 @@ class ConverterMasked(ConverterBase):
                         if self.final_image_color_degrade_power == 100:
                             out_img = out_img_reduced
                         else:
-                            alpha = self.final_image_color_degrade_power / 100.0                    
+                            alpha = self.final_image_color_degrade_power / 100.0
                             out_img = (out_img*(1.0-alpha) + out_img_reduced*alpha)
                         
                     if self.alpha:
