c311f09fa4531bbdf9733f28141089cfd264a3e0
iperov <lepersorium@gmail.com>
2019-03-17 21:14:24 +0400

    _

--- facelib/__init__.py
+++ facelib/__init__.py
@@ -3,3 +3,4 @@ from .DLIBExtractor import DLIBExtractor
 from .MTCExtractor import MTCExtractor
 from .S3FDExtractor import S3FDExtractor
 from .LandmarksExtractor import LandmarksExtractor
+from .FANSegmentator import FANSegmentator
