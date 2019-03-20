6169e6ba8a4531a3c9b316ae40fd8f5cc5b268ab
iperov <lepersorium@gmail.com>
2019-03-20 09:08:42 +0400

    fix fanseg

--- models/Model_FANSegmentator/Model.py
+++ models/Model_FANSegmentator/Model.py
@@ -10,13 +10,13 @@ from interact import interact as io
 class Model(ModelBase):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs,
-                            ask_write_preview_history=False,
+        super().__init__(*args, **kwargs, 
+                            ask_write_preview_history=False, 
                             ask_target_iter=False,
                             ask_sort_by_yaw=False,
                             ask_random_flip=False,
                             ask_src_scale_mod=False)
-
+        
     #override
     def onInitialize(self):
         exec(nnlib.import_all(), locals(), globals())
@@ -24,33 +24,34 @@ class Model(ModelBase):
 
         self.resolution = 256
         self.face_type = FaceType.FULL
-
-        self.fan_seg = FANSegmentator(self.resolution,
-                                      FaceType.toString(self.face_type),
+        
+        self.fan_seg = FANSegmentator(self.resolution, 
+                                      FaceType.toString(self.face_type), 
                                       load_weights=not self.is_first_run(),
-                                      weights_file_root=self.get_model_root_path() )
+                                      weights_file_root=self.get_model_root_path(),
+                                      training=True)
 
         if self.is_training_mode:
             f = SampleProcessor.TypeFlags
             f_type = f.FACE_ALIGN_FULL
-
-            self.set_training_data_generators ([
-                    SampleGeneratorFace(self.training_data_src_path, debug=self.is_debug(), batch_size=self.batch_size,
-                            sample_process_options=SampleProcessor.Options(random_flip=True, normalize_tanh = True ),
+            
+            self.set_training_data_generators ([    
+                    SampleGeneratorFace(self.training_data_src_path, debug=self.is_debug(), batch_size=self.batch_size, 
+                            sample_process_options=SampleProcessor.Options(random_flip=True, normalize_tanh = True ), 
                             output_sample_types=[ [f.TRANSFORMED | f_type | f.MODE_BGR_SHUFFLE, self.resolution],
                                                   [f.TRANSFORMED | f_type | f.MODE_M | f.FACE_MASK_FULL, self.resolution]
                                                 ]),
-
-                    SampleGeneratorFace(self.training_data_dst_path, debug=self.is_debug(), batch_size=self.batch_size,
-                            sample_process_options=SampleProcessor.Options(random_flip=True, normalize_tanh = True ),
+                                                
+                    SampleGeneratorFace(self.training_data_dst_path, debug=self.is_debug(), batch_size=self.batch_size, 
+                            sample_process_options=SampleProcessor.Options(random_flip=True, normalize_tanh = True ), 
                             output_sample_types=[ [f.TRANSFORMED | f_type | f.MODE_BGR_SHUFFLE, self.resolution]
                                                 ])
                                                ])
-
+                
     #override
-    def onSave(self):
+    def onSave(self):        
         self.fan_seg.save_weights()
-
+        
     #override
     def onTrainOneIter(self, generators_samples, generators_list):
         target_src, target_src_mask = generators_samples[0]
@@ -58,20 +59,20 @@ class Model(ModelBase):
         loss = self.fan_seg.train_on_batch( [target_src], [target_src_mask] )
 
         return ( ('loss', loss), )
-
+        
     #override
     def onGetPreview(self, sample):
         test_A   = sample[0][0][0:4] #first 4 samples
         test_B   = sample[1][0][0:4] #first 4 samples
-
+        
         mAA = self.fan_seg.extract_from_bgr([test_A])
         mBB = self.fan_seg.extract_from_bgr([test_B])
-
+        
         test_A, test_B, = [ np.clip( (x + 1.0)/2.0, 0.0, 1.0)  for x in [test_A, test_B] ]
-
+        
         mAA = np.repeat ( mAA, (3,), -1)
         mBB = np.repeat ( mBB, (3,), -1)
-
+        
         st = []
         for i in range(0, len(test_A)):
             st.append ( np.concatenate ( (
@@ -79,7 +80,7 @@ class Model(ModelBase):
                 mAA[i],
                 test_A[i,:,:,0:3]*mAA[i],
                 ), axis=1) )
-
+                
         st2 = []
         for i in range(0, len(test_B)):
             st2.append ( np.concatenate ( (
@@ -87,7 +88,7 @@ class Model(ModelBase):
                 mBB[i],
                 test_B[i,:,:,0:3]*mBB[i],
                 ), axis=1) )
-
+                
         return [ ('FANSegmentator', np.concatenate ( st, axis=0 ) ),
                  ('never seen', np.concatenate ( st2, axis=0 ) ),
                  ]
