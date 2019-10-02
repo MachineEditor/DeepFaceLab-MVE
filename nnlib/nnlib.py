353bcdf80f34a59ce22a09802847dac428a739b3
Colombo <lepersorium@gmail.com>
2019-10-02 20:17:56 +0400

    cuda_cache_max_size

--- nnlib/nnlib.py
+++ nnlib/nnlib.py
@@ -139,6 +139,8 @@ NLayerDiscriminator = nnlib.NLayerDiscriminator
 
         if 'CUDA_VISIBLE_DEVICES' in os.environ.keys():
             os.environ.pop('CUDA_VISIBLE_DEVICES')
+            
+        os.environ['CUDA_​CACHE_​MAXSIZE'] = '536870912' #512Mb (32mb default)
 
         os.environ['TF_MIN_GPU_MULTIPROCESSOR_COUNT'] = '2'
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #tf log errors only
