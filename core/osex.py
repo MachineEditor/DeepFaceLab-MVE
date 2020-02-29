c900c65c31556d9be842c39bb5391fbe3123f9f0
Colombo <lepersorium@gmail.com>
2020-02-29 15:20:17 +0400

    _

--- core/osex.py
+++ core/osex.py
@@ -41,4 +41,4 @@ def linux_ignore_UserWarning():
         # /usr/lib/python3.6/multiprocessing/semaphore_tracker.py:143: 
         # UserWarning: semaphore_tracker: There appear to be 1 leaked semaphores to clean up at shutdown
         import warnings
-        warnings.filterwarnings(action='ignore', message='semaphore')
+        warnings.filterwarnings(action='ignore', message='semaphore_tracker')
