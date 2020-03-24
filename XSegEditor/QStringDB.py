54fdeb0666912863c313f21d205d7ce294216aa7
IMIYOU <58950963+IMIYOU@users.noreply.github.com>
2020-03-25 04:40:07 +0800

    fix Chinesse language (#672)

--- XSegEditor/QStringDB.py
+++ XSegEditor/QStringDB.py
@@ -12,17 +12,17 @@ class QStringDB():
         
         QStringDB.btn_poly_color_red_tip = {    'en' : 'Poly color scheme red',
                                                 'ru' : 'Красная цветовая схема полигонов',
-                                                'zh' : '多边形配色方案红色',
+                                                'zh' : '选区配色方案红色',
                                            }[lang]
                                            
         QStringDB.btn_poly_color_green_tip = {  'en' : 'Poly color scheme green',
                                                 'ru' : 'Зелёная цветовая схема полигонов',
-                                                'zh' : '多边形配色方案绿色',
+                                                'zh' : '选区配色方案绿色',
                                            }[lang]
                                            
         QStringDB.btn_poly_color_blue_tip = {   'en' : 'Poly color scheme blue',
                                                 'ru' : 'Синяя цветовая схема полигонов',
-                                                'zh' : '多边形配色方案蓝色',
+                                                'zh' : '选区配色方案蓝色',
                                            }[lang]
                                            
         QStringDB.btn_view_baked_mask_tip = {   'en' : 'View baked mask',
@@ -32,12 +32,12 @@ class QStringDB():
                                            
         QStringDB.btn_poly_type_include_tip = { 'en' : 'Poly include mode',
                                                 'ru' : 'Режим полигонов - включение',
-                                                'zh' : '多边形包含模式',
+                                                'zh' : '包含选区模式',
                                            }[lang]
                                            
         QStringDB.btn_poly_type_exclude_tip = { 'en' : 'Poly exclude mode',
                                                 'ru' : 'Режим полигонов - исключение',
-                                                'zh' : '多边形排除方式',
+                                                'zh' : '排除选区模式',
                                            }[lang]        
                                            
         QStringDB.btn_undo_pt_tip = {   'en' : 'Undo point',
@@ -52,7 +52,7 @@ class QStringDB():
                                       
         QStringDB.btn_delete_poly_tip = {   'en' : 'Delete poly',
                                             'ru' : 'Удалить полигон',
-                                            'zh' : '删除多边形',
+                                            'zh' : '删除选区',
                                            }[lang]     
                                               
         QStringDB.btn_pt_edit_mode_tip = {  'en' : 'Edit point mode ( HOLD CTRL )',
@@ -62,11 +62,11 @@ class QStringDB():
                                            
         QStringDB.btn_prev_image_tip = {    'en' : 'Save and Prev image\nHold SHIFT : accelerate\nHold CTRL : skip non masked\n',
                                             'ru' : 'Сохранить и предыдущее изображение\nУдерживать SHIFT : ускорить\nУдерживать CTRL : пропустить неразмеченные\n',
-                                            'zh' : '保存和上一张图片\n按住SHIFT : 加快\n按住CTRL : 跳过未标记的\n',
+                                            'zh' : '保存并转到上一张图片\n按住SHIFT : 加快\n按住CTRL : 跳过未标记的\n',
                                            }[lang]   
         QStringDB.btn_next_image_tip = {    'en' : 'Save and Next image\nHold SHIFT : accelerate\nHold CTRL : skip non masked\n',
                                             'ru' : 'Сохранить и следующее изображение\nУдерживать SHIFT : ускорить\nУдерживать CTRL : пропустить неразмеченные\n',
-                                            'zh' : '保存并下一张图片\n按住SHIFT : 加快\n按住CTRL : 跳过未标记的\n',
+                                            'zh' : '保存并转到下一张图片\n按住SHIFT : 加快\n按住CTRL : 跳过未标记的\n',
                                            }[lang]  
                                            
                                            
