703c790e67d110e047a1750fd2d6f34cb5085fc1
jh <jmhummel@gmail.com>
2021-03-13 14:39:36 -0800

    kernel size fix

--- core/leras/layers/MsSsim.py
+++ core/leras/layers/MsSsim.py
@@ -20,7 +20,7 @@ class MsSsim(nn.LayerBase):
         y_true_t = tf.transpose(tf.cast(y_true, tf.float32), [0, 2, 3, 1])
         y_pred_t = tf.transpose(tf.cast(y_pred, tf.float32), [0, 2, 3, 1])
 
-        loss = tf.image.ssim_multiscale(y_true_t, y_pred_t, max_val, power_factors=self.power_factors)
+        loss = tf.image.ssim_multiscale(y_true_t, y_pred_t, max_val, power_factors=self.power_factors, filter_size=self.kernel_size)
         return (1.0 - loss) / 2.0
 
 nn.MsSsim = MsSsim
