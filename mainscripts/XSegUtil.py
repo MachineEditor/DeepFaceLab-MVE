1652bffeb0bb242dc06516880f754abfea1e207e
iperov <lepersorium@gmail.com>
2021-03-24 13:42:57 +0400

    change in xseg apply and fetch

--- mainscripts/XSegUtil.py
+++ mainscripts/XSegUtil.py
@@ -10,7 +10,7 @@ from core.cv2ex import *
 from core.interact import interact as io
 from core.leras import nn
 from DFLIMG import *
-from facelib import XSegNet
+from facelib import XSegNet, LandmarksProcessor, FaceType
 
 
 def apply_xseg(input_path, model_path):
@@ -19,7 +19,16 @@ def apply_xseg(input_path, model_path):
 
     if not model_path.exists():
         raise ValueError(f'{model_path} not found. Please ensure it exists.')
-        
+
+    face_type = io.input_str ("XSeg model face type", 'same', ['h','mf','f','wf','head','same'], help_message="Specify face type of trained XSeg model. For example if XSeg model trained as WF, but faceset is HEAD, specify WF to apply xseg only on WF part of HEAD. Default is 'same'").lower()
+    if face_type == 'same':
+        face_type = None
+    else:
+        face_type = {'h'  : FaceType.HALF,
+                     'mf' : FaceType.MID_FULL,
+                     'f'  : FaceType.FULL,
+                     'wf' : FaceType.WHOLE_FACE,
+                     'head' : FaceType.HEAD}[face_type]
     io.log_info(f'Applying trained XSeg model to {input_path.name}/ folder.')
 
     device_config = nn.DeviceConfig.ask_choose_device(choose_only_one=True)
@@ -30,7 +39,7 @@ def apply_xseg(input_path, model_path):
                     weights_file_root=model_path,
                     data_format=nn.data_format,
                     raise_on_no_model_files=True)
-    res = xseg.get_resolution()
+    xseg_res = xseg.get_resolution()
               
     images_paths = pathex.get_image_paths(input_path, return_Path_class=True)
     
@@ -42,15 +51,36 @@ def apply_xseg(input_path, model_path):
         
         img = cv2_imread(filepath).astype(np.float32) / 255.0
         h,w,c = img.shape
-        if w != res:
-            img = cv2.resize( img, (res,res), interpolation=cv2.INTER_CUBIC )        
-            if len(img.shape) == 2:
-                img = img[...,None]            
-            
-        mask = xseg.extract(img)
-        mask[mask < 0.5]=0
-        mask[mask >= 0.5]=1
         
+        img_face_type = FaceType.fromString( dflimg.get_face_type() )
+        if face_type is not None and img_face_type != face_type:
+            lmrks = dflimg.get_source_landmarks()
+            
+            fmat = LandmarksProcessor.get_transform_mat(lmrks, w, face_type)
+            imat = LandmarksProcessor.get_transform_mat(lmrks, w, img_face_type)
+            
+            g_p = LandmarksProcessor.transform_points (np.float32([(0,0),(w,0),(0,w) ]), fmat, True)
+            g_p2 = LandmarksProcessor.transform_points (g_p, imat)
+            
+            mat = cv2.getAffineTransform( g_p2, np.float32([(0,0),(w,0),(0,w) ]) )
+            
+            img = cv2.warpAffine(img, mat, (w, w), cv2.INTER_LANCZOS4)
+            img = cv2.resize(img, (xseg_res, xseg_res), interpolation=cv2.INTER_LANCZOS4)
+        else:
+            if w != xseg_res:
+                img = cv2.resize( img, (xseg_res,xseg_res), interpolation=cv2.INTER_LANCZOS4 )    
+                    
+        if len(img.shape) == 2:
+            img = img[...,None]            
+    
+        mask = xseg.extract(img)
+        
+        if face_type is not None and img_face_type != face_type:
+            mask = cv2.resize(mask, (w, w), interpolation=cv2.INTER_LANCZOS4)
+            mask = cv2.warpAffine( mask, mat, (w,w), np.zeros( (h,w,c), dtype=np.float), cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4)
+            mask = cv2.resize(mask, (xseg_res, xseg_res), interpolation=cv2.INTER_LANCZOS4)
+        mask[mask < 0.5]=0
+        mask[mask >= 0.5]=1    
         dflimg.set_xseg_mask(mask)
         dflimg.save()
 
@@ -67,7 +97,8 @@ def fetch_xseg(input_path):
     
     images_paths = pathex.get_image_paths(input_path, return_Path_class=True)
     
-    files_copied = 0
+    
+    files_copied = []
     for filepath in io.progress_bar_generator(images_paths, "Processing"):
         dflimg = DFLIMG.load(filepath)
         if dflimg is None or not dflimg.has_data():
@@ -77,10 +108,16 @@ def fetch_xseg(input_path):
         ie_polys = dflimg.get_seg_ie_polys()
 
         if ie_polys.has_polys():
-            files_copied += 1
+            files_copied.append(filepath)
             shutil.copy ( str(filepath), str(output_path / filepath.name) )
     
-    io.log_info(f'Files copied: {files_copied}')
+    io.log_info(f'Files copied: {len(files_copied)}')
+    
+    is_delete = io.input_bool (f"\r\nDelete original files?", True)
+    if is_delete:
+        for filepath in files_copied:
+            Path(filepath).unlink()
+            
     
 def remove_xseg(input_path):
     if not input_path.exists():
