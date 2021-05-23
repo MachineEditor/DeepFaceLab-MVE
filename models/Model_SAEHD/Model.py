ba34a65a87a5b82c31e20d0c7a0cc73a2caf4f09
Jeremy Hummel <jmhummel@gmail.com>
2021-05-22 23:23:46 -0700

    Set default value for random downsampling

--- models/Model_SAEHD/Model.py
+++ models/Model_SAEHD/Model.py
@@ -56,6 +56,7 @@ class SAEHDModel(ModelBase):
         default_loss_function      = self.options['loss_function']      = self.load_or_def_option('loss_function', 'SSIM')
 
         default_random_warp        = self.options['random_warp']        = self.load_or_def_option('random_warp', True)
+        default_random_downsample  = self.options['random_downsample']  = self.load_or_def_option('random_downsample', False)
         default_background_power   = self.options['background_power']   = self.load_or_def_option('background_power', 0.0)
         default_true_face_power    = self.options['true_face_power']    = self.load_or_def_option('true_face_power', 0.0)
         default_face_style_power   = self.options['face_style_power']   = self.load_or_def_option('face_style_power', 0.0)
@@ -159,10 +160,10 @@ Examples: df, liae, df-d, df-ud, liae-ud, ...
 
             self.options['random_warp'] = io.input_bool ("Enable random warp of samples", default_random_warp, help_message="Random warp is required to generalize facial expressions of both faces. When the face is trained enough, you can disable it to get extra sharpness and reduce subpixel shake for less amount of iterations.")
 
-            self.options['random_downsample'] = io.input_bool("Enable random downsample of samples", False, help_message="")
-            self.options['random_noise'] = io.input_bool("Enable random noise added to samples", False, help_message="")
-            self.options['random_blur'] = io.input_bool("Enable random blur of samples", False, help_message="")
-            self.options['random_jpeg'] = io.input_bool("Enable random jpeg compression of samples", False, help_message="")
+            self.options['random_downsample'] = io.input_bool("Enable random downsample of samples", default_random_downsample, help_message="")
+            # self.options['random_noise'] = io.input_bool("Enable random noise added to samples", False, help_message="")
+            # self.options['random_blur'] = io.input_bool("Enable random blur of samples", False, help_message="")
+            # self.options['random_jpeg'] = io.input_bool("Enable random jpeg compression of samples", False, help_message="")
 
             self.options['gan_version'] = np.clip (io.input_int("GAN version", default_gan_version, add_info="2 or 3", help_message="Choose GAN version (v2: 7/16/2020, v3: 1/3/2021):"), 2, 3)
 
