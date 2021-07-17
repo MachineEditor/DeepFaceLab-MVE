9ad9728b4021d1dff62905cce03e2157d0c0868d
iperov <lepersorium@gmail.com>
2021-07-17 22:04:34 +0400

    fix

--- mainscripts/ExportDFM.py
+++ mainscripts/ExportDFM.py
@@ -16,7 +16,7 @@ from core.interact import interact as io
 
 def main(model_class_name, saved_models_path):
     model = models.import_model(model_class_name)(
-                        is_training=False,
+                        is_exporting=True,
                         saved_models_path=saved_models_path,
                         cpu_only=True)
     model.export_dfm () 
