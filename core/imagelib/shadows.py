4bf6d3d7b7977324da9b6eda4bfc2d0cdbbcb1d6
seranus <ognjen_j@yahoo.com>
2021-12-10 19:09:08 +0100

    shadows - value tweaking

--- core/imagelib/shadows.py
+++ core/imagelib/shadows.py
@@ -7,11 +7,11 @@ import cv2
 def shadow_highlights_augmentation(img, seed=None):
     rnd_state = np.random.RandomState (seed)
 
-    high_ratio = (1, 3)
+    high_ratio = (1, 2.5)
     low_ratio = (0.2, 0.6)
-    left_low_ratio = (0.4, 0.7)
+    left_low_ratio = (0.2, 0.6)
     left_high_ratio = (0, 0.2)
-    right_low_ratio = (0.4, 0.7)
+    right_low_ratio = (0.3, 0.6)
     right_high_ratio = (0, 0.2)
 
     # check
