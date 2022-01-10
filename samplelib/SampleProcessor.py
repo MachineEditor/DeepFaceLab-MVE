9bf3eb5851c6d0fbd2cea201332a60b047bb9113
Cioscos <claudiocicca1998@gmail.com>
2022-01-10 20:57:38 +0100

    Splittable random shadows
    - Now user can decide to apply random shadows aug just to src or to dst
      or both of them.
    - Refactoring

--- samplelib/SampleProcessor.py
+++ samplelib/SampleProcessor.py
@@ -266,6 +266,7 @@ class SampleProcessor(object):
                             img_v = np.clip (img_v + (rnd_state.random()-0.5)*a, 0, 1 )
                             img = np.clip( cv2.cvtColor(cv2.merge([img_h, img_s, img_v]), cv2.COLOR_HSV2BGR) , 0, 1 )
                         
+                        # Apply random shadow
                         if random_shadow == True and sample_rnd_seed % 10 / 10 < 0.5:
                             img = shadow_highlights_augmentation(img, sample_rnd_seed)
                         img  = imagelib.warp_by_params (warp_params, img,  warp, transform, can_flip=True, border_replicate=border_replicate)
