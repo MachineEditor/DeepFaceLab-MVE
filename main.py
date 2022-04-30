22595201362654cd36e87b5ccf850e662244c106
Cioscos <claudiocicca1998@gmail.com>
2022-04-30 18:39:34 +0200

    Removed wrong merged code

--- main.py
+++ main.py
@@ -179,16 +179,6 @@ if __name__ == "__main__":
     p.add_argument('--model-dir', required=True, action=fixPathAction, dest="model_dir", help="Saved models dir.")
     p.add_argument('--model', required=True, dest="model_name", choices=pathex.get_all_dir_names_startswith ( Path(__file__).parent / 'models' , 'Model_'), help="Model class name.")
     p.set_defaults (func=process_exportdfm)
-
-    def process_exportdfm(arguments):
-        osex.set_process_lowest_prio()
-        from mainscripts import ExportDFM
-        ExportDFM.main(model_class_name = arguments.model_name, saved_models_path = Path(arguments.model_dir))
-
-    p = subparsers.add_parser( "exportdfm", help="Export model to use in DeepFaceLive.")
-    p.add_argument('--model-dir', required=True, action=fixPathAction, dest="model_dir", help="Saved models dir.")
-    p.add_argument('--model', required=True, dest="model_name", choices=pathex.get_all_dir_names_startswith ( Path(__file__).parent / 'models' , 'Model_'), help="Model class name.")
-    p.set_defaults (func=process_exportdfm)
     
     def process_merge(arguments):
         osex.set_process_lowest_prio()
